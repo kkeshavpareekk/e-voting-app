e9eadf4589527be5ee8f2de5de5ef189ed9eb0e4
keshav pareek <kkeshavpareekk@gmail.com>
2023-02-24 10:32:10 +0530

    final changes

---
+++ src/component/Navbar/Navigation.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+import "./Navbar.css";
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <NavLink to="/" className="header">
+        Home
+      </NavLink>
+      <ul
+        className="navbar-links"
+        style={{ width: "35%", transform: open ? "translateX(0px)" : "" }}
+      >
+        <li>
+          <NavLink to="/Registration" activeClassName="nav-active">
+            Registration
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Voting" activeClassName="nav-active">
+            Voting
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/Results" activeClassName="nav-active">
+            Results
+          </NavLink>
+        </li>
+      </ul>
+      <i onClick={() => setOpen(!open)} className="fas fa-bars burger-menu"></i>
+    </nav>
+  );
+}
---
+++ src/component/NotInit.js
@@ -0,0 +1,14 @@
+// Node module
+import React from "react";
+
+const NotInit = () => {
+  return (
+    <div className="container-item info">
+      <center>
+        <h3>The election has not been initialize.</h3>
+        <p>Please Wait..</p>
+      </center>
+    </div>
+  );
+};
+export default NotInit;
